ff7198db5c79501e557ef43103a26c85f0b9ddca
Sagi Shnaidman <sshnaidm@redhat.com>
2016-09-15 11:59:34 +0300

    first
    Change-Id: I43d07a089fa6e80b38530f1a9c0b6dcae532b2dd

---
+++ toci_instack_oooq.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -eux
+
+## Signal to toci_gate_test.sh we've started
+touch /tmp/toci.started
+
+export CURRENT_DIR=$(dirname ${BASH_SOURCE[0]:-$0})
+export TRIPLEO_CI_DIR=$CURRENT_DIR/../
+
+source $TRIPLEO_CI_DIR/tripleo-ci/scripts/common_vars.bash
+source $TRIPLEO_CI_DIR/tripleo-ci/scripts/common_functions.sh
+#source $TRIPLEO_CI_DIR/tripleo-ci/scripts/metrics.bash
+
+mkdir -p $WORKSPACE/logs
+
+hostname | sudo dd of=/etc/hostname
+echo "127.0.0.1 $(hostname) $(hostname).openstacklocal" | sudo tee -a /etc/hosts
+echo "127.0.0.2 $(hostname) $(hostname).openstacklocal" | sudo tee -a /etc/hosts
+
+echo | sudo tee -a ~root/.ssh/authorized_keys | sudo tee -a ~/.ssh/authorized_keys
+if [ ! -e /home/$USER/.ssh/id_rsa.pub ] ; then
+    ssh-keygen -N "" -f /home/$USER/.ssh/id_rsa
+fi
+cat ~/.ssh/id_rsa.pub | sudo tee -a ~root/.ssh/authorized_keys | sudo tee -a ~/.ssh/authorized_keys
+
+sudo yum remove -y puppet hiera puppetlabs-release rdo-release
+sudo rm -rf /etc/puppet /etc/hiera.yaml
+
+trap "[ \$? != 0 ] && echo ERROR DURING PREVIOUS COMMAND ^^^ && echo 'See postci.txt in the logs directory for debugging details'; postci 2>&1 | ts '%Y-%m-%d %H:%M:%S.000 |' > $WORKSPACE/logs/postci.log 2>&1" EXIT
+
+# Install our test cert so SSL tests work
+sudo cp $TRIPLEO_ROOT/tripleo-ci/test-environments/overcloud-cacert.pem /etc/pki/ca-trust/source/anchors/
+sudo update-ca-trust extract
+
+cp -f $TE_DATAFILE ~/instackenv.json
+
