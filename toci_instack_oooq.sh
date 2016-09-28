b2e4e786847a894a97e14fe2f2c4670b6189ee20
Sagi Shnaidman <sshnaidm@redhat.com>
2016-09-28 07:14:40 +0300

    Test 5
    Change-Id: Ifdbff689afaf546e5ae9980813b968172c97fd21

--- toci_instack_oooq.sh
+++ toci_instack_oooq.sh
@@ -65,7 +65,7 @@ echo "$TRIPLEO_ROOT/tripleo-quickstart/quickstart.sh  --bootstrap \
 
 $TRIPLEO_ROOT/tripleo-quickstart/quickstart.sh  --bootstrap --no-clone \
         -t all \
-        $PLAYBOOK $UNDERCLOUD_SCRIPTS \
+        $PLAYBOOK $UNDERCLOUD_SCRIPTS -e step_adjust_mtu=True \
         $OOOQ_DEFAULT_ARGS 127.0.0.2 2>&1 \
         | ts '%Y-%m-%d %H:%M:%S.000 |' | sudo tee /var/log/undercloud_install.txt ||:
 
